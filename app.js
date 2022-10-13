84796db999c80197d34c5e7b97ba39de0a0820e1
kimyena1123 <kyq104@gmail.com>
2022-10-13 14:22:47 +0900

    study : first commit

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = 8300; // 포트번호
+
+app.set('view engine', 'ejs'); //app에 view engine을 ejs로 설정
+app.use('/views', express.static(__dirname + '/views')); // ejs 파일을 저장할 views 폴더의 경로
+
+//브라우저가 ip:port/stztic/image/cat.jpg
+//해당 파일을 ip:port/static/image/cat.jpg 에 존재하는지 찾음
+app.use('/static', express.static(__dirname + '/static'));
+
+app.get('/',function(request, response){
+    //client가 get 요청을 보내면 -> 응답으로 send('안에 있는 메서지')를 띄운다
+    response.send('connect success');
+})
+
+app.get('/test', function(request, response){
+    response.render('test');
+})
+
+// app.get('/test2', function (request, response) {
+//     //   response.send('test page');
+//       response.render('test2');
+//     })
+    
+//로컬 서버 동작
+app.listen(PORT, function(){
+    console.log(`Listening on port ${PORT}`);
+})
