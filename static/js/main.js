492e8910ad45852840d4a48ea2b5e1d5b30bdabc
mangrang <amole2315@naver.com>
2022-10-17 15:48:45 +0900

    fix: festival

--- static/js/main.js
+++ static/js/main.js
@@ -1,4 +1,6 @@
-function changeThema (){
+// 서브 사진테마 효과
+
+function changeThema() {
     $('.themaFoods').addClass('hidden')
     $('.themaGrims').removeClass('hidden')
     $('.themBtn').addClass('cliBtn')
@@ -6,10 +8,10 @@ function changeThema (){
 
     $('.foodBtn').addClass('unCliBtn')
     $('.themBtn').removeClass('unCliBtn')
-    
+
 }
 
-function changeFood (){
+function changeFood() {
     $('.themaGrims').addClass('hidden')
     $('.themaFoods').removeClass('hidden')
     $('.foodBtn').addClass('cliBtn')
@@ -17,7 +19,47 @@ function changeFood (){
 
     $('.themBtn').addClass('unCliBtn')
     $('.foodBtn').removeClass('unCliBtn')
-    
 }
+
+// 페스티벌 사진 호버시 효과
+
+$('.fstvAni').mouseover(function () {
+    let festIcon = $(this).find('.icons');
+    festIcon.addClass('transIcon');
+})
+
+$('.fstvAni').mouseout(function () {
+    let festIcon = $(this).find('.icons');
+    festIcon.removeClass('transIcon');
+})
+
+// 페스티벌 사진 클릭시 효과
+
+
+$('.fstvDiv').click(function () {
+    
+    // $('.fstvDiv').addClass('hidden');
+    $(this).addClass('fstvCliSt')
+    
+    $(this).removeClass('hidden');
+    let test = $(this);
+    setTimeout(function () {
+        // console.log($(this))
+        // $(this).removeClass('fstvAni');
+        // $(this).addClass('fstvCli')
+        // $(this).next('.fstvDesc').removeClass('hidden');
+   
+        test.removeClass('fstvAni');
+        test.addClass('fstvCli')
+        test.next('.fstvDesc').removeClass('hidden');
+    }, 2000);
+
+    $('.fstvDiv').addClass('hidden');
+    $(this).removeClass('hidden');
+    
+    // $(this).removeClass('fstvAni');
+    // $(this).addClass('fstvCli')
+    // $(this).next('.fstvDesc').removeClass('hidden');
+})
 
 
--- static/js/nav.js
+++ static/js/nav.js
@@ -1,3 +1,5 @@
+// 로그인창 기능
+
 $('.nickForm').submit(function(e){
     e.preventDefault();
     let nickVal = $('.nickInput').val();
@@ -12,14 +14,13 @@ $('.nickForm').submit(function(e){
     console.log(nickVal);
 })
 
+// 네비게이션 마우스 호버시 나타나는 효과
+
 $('.navDiv').mouseover(function(){
     $('.navDiv').removeClass('navHidDiv')
-
-    
 })
 
 $('.navDiv').mouseout(function(){
     $('.navDiv').addClass('navHidDiv')
-
-    
 })
+
