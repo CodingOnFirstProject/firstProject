1a31e03a8be56a703a024ed0b2a125b7fdce1588
kimyena1123 <kyq104@gmail.com>
2022-10-23 21:26:50 +0900

    fix: 날씨창 js 수정

--- static/js/pageWeather.js
+++ static/js/pageWeather.js
@@ -9,32 +9,28 @@ let humidity = document.querySelector("#humidity");
 let wind = document.querySelector("#wind");
 
 const cityName = document.querySelector("#cityName"); // 도시 이름을 저장한 변수
+
 const resultbtn = document.querySelector('#resultbtn');
+const btnMax = document.querySelector(".btnMax");
+const btnMin = document.querySelector(".btnMin");
 
-resultbtn.addEventListener("click", apply); // "날씨보기" 버튼을 누르면 showResult()함수가 실행
+const tempMaxMinName = document.querySelector(".tempMaxMinName");
 
-function apply(){
-    //cityurax = `'${cityValue}'`; // 버튼을 누르면 그 해당 도시의 날씨정보가 보이게
-    //cityurax의 값을 기본값(seoul)에서 사용자가 고른 값(해당 도시)으로 바꿔준다.
+// resultbtn.addEventListener("click", apply); // "날씨보기" 버튼을 누르면 showResult()함수가 실행
 
-
-    const cityValue = city.value; // city 변수의 value값을 cityValue에 대입.
-
-    cityName.innerHTML = cityValue;
-    console.log("cityvalue 확인 : " + cityValue); 
-    console.log("다시확인: " + `'${cityValue}'`);
-    // console.log("cityName 확인: " + cityName);
-}
+// function apply(){
+//     //cityurax = `'${cityValue}'`; // 버튼을 누르면 그 해당 도시의 날씨정보가 보이게
+//     //cityurax의 값을 기본값(seoul)에서 사용자가 고른 값(해당 도시)으로 바꿔준다.
 
 
+//     const cityValue = city.value; // city 변수의 value값을 cityValue에 대입.
 
-let nowTemp = document.querySelector('#nowTemp');
-let nowHumidity = document.querySelector('#nowHumidity')
-let weather = document.querySelector('#weather');
-// let wind = document.querySelector('#wind');
-let country = document.querySelector('#country');
-// let city = document.querySelector('#city');
-let weatherimg = document.querySelector('img');
+//     cityName.innerHTML = cityValue;
+//     console.log("cityvalue 확인 : " + cityValue); 
+//     console.log("다시확인: " + `'${cityValue}'`);
+//     // console.log("cityName 확인: " + cityName);
+// }
+
 cityurax = 'seoul'; //서울은 기본값
 // cityurax = `'${cityValue}'`;
 
@@ -49,6 +45,7 @@ $.ajax({
     console.log('원래 현재온도 : ' + (response.main.temp - 273.15));
     console.log('최고온도 : ' + (response.main.temp_max - 273.15));
     console.log('최저 온도: ' + (response.main.temp_min - 273.15));
+
     console.log('현재습도 : ' + response.main.humidity);
     console.log('날씨 : ' + response.weather[0].main);
     console.log('상세날씨설명 : ' + response.weather[0].description);
@@ -61,6 +58,27 @@ $.ajax({
     temp.innerHTML = `${Math.floor(response.main.temp - 273.15)}°C`;
     humidity.innerHTML = response.main.humidity;
     wind.innerHTML = response.wind.speed;
+
+    resultbtn.addEventListener("click", function(){
+
+      const cityValue = city.value;
+      cityName.innerHTML = cityValue;
+
+
+      console.log("cityvalue 확인 : " + cityValue); 
+      console.log("다시확인: " + `'${cityValue}'`);
+    })
+
+    btnMax.addEventListener("click", function(){
+      tempMaxMinName.innerHTML = "최고온도";
+      temp.innerHTML = `${Math.floor(response.main.temp_max - 273.15)}°C`;
+    })
+
+    btnMin.addEventListener("click", function(){
+      tempMaxMinName.innerHTML = "최저온도";
+      temp.innerHTML = `${Math.floor(response.main.temp_min - 273.15)}°C`;
+    })
+
 
     
   },
