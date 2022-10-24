bb5dab202ba9aa216944c055e3f797721ef77803
kimyena1123 <kyq104@gmail.com>
2022-10-24 16:12:45 +0900

    fix: color, 레이아웃 수정

--- static/js/pageWeather.js
+++ static/js/pageWeather.js
@@ -13,73 +13,103 @@ const cityName = document.querySelector("#cityName"); // 도시 이름을 저장
 const resultbtn = document.querySelector('#resultbtn');
 const btnMax = document.querySelector(".btnMax");
 const btnMin = document.querySelector(".btnMin");
+const now = document.querySelector('.now');
 
 const tempMaxMinName = document.querySelector(".tempMaxMinName");
 
-// resultbtn.addEventListener("click", apply); // "날씨보기" 버튼을 누르면 showResult()함수가 실행
+let cityDay = document.querySelector(".cityDay");
 
-// function apply(){
-//     //cityurax = `'${cityValue}'`; // 버튼을 누르면 그 해당 도시의 날씨정보가 보이게
-//     //cityurax의 값을 기본값(seoul)에서 사용자가 고른 값(해당 도시)으로 바꿔준다.
+let clock = new Date();
+clock.setHours(clock.getHours() - 7);
 
+var time = clock;
 
-//     const cityValue = city.value; // city 변수의 value값을 cityValue에 대입.
+var year = time.getFullYear();
+var month = time.getMonth() + 1;
+var date = time.getDate();
 
-//     cityName.innerHTML = cityValue;
-//     console.log("cityvalue 확인 : " + cityValue); 
-//     console.log("다시확인: " + `'${cityValue}'`);
-//     // console.log("cityName 확인: " + cityName);
-// }
-
-cityurax = 'seoul'; //서울은 기본값
-// cityurax = `'${cityValue}'`;
-
-let url = API_URL + cityurax + '&appid=' + API_KEY;
-$.ajax({
-  url: url,
-  dataType: 'json',
-  type: 'GET',
-  async: 'false',
-  success: function (response) {
-    console.log(response);
-    console.log('원래 현재온도 : ' + (response.main.temp - 273.15));
-    console.log('최고온도 : ' + (response.main.temp_max - 273.15));
-    console.log('최저 온도: ' + (response.main.temp_min - 273.15));
-
-    console.log('현재습도 : ' + response.main.humidity);
-    console.log('날씨 : ' + response.weather[0].main);
-    console.log('상세날씨설명 : ' + response.weather[0].description);
-    console.log('날씨 이미지 : ' + response.weather[0].icon);
-    console.log('바람   : ' + response.wind.speed);
-    console.log('나라   : ' + response.sys.country);
-    console.log('도시이름  : ' + response.name);
-    console.log('구름  : ' + response.clouds.all + '%');
-
-    temp.innerHTML = `${Math.floor(response.main.temp - 273.15)}°C`;
-    humidity.innerHTML = response.main.humidity;
-    wind.innerHTML = response.wind.speed;
-
-    resultbtn.addEventListener("click", function(){
-
-      const cityValue = city.value;
-      cityName.innerHTML = cityValue;
+cityDay.innerText = `${year}년 ${month}월 ${date}일`;
 
 
-      console.log("cityvalue 확인 : " + cityValue); 
-      console.log("다시확인: " + `'${cityValue}'`);
-    })
+resultbtn.addEventListener("click", apply);
 
-    btnMax.addEventListener("click", function(){
-      tempMaxMinName.innerHTML = "최고온도";
-      temp.innerHTML = `${Math.floor(response.main.temp_max - 273.15)}°C`;
-    })
+function apply(){
+  const cityValue = city.value; // city 변수의 value값을 cityValue에 대입.
+  const name = `${cityValue}`;
 
-    btnMin.addEventListener("click", function(){
-      tempMaxMinName.innerHTML = "최저온도";
-      temp.innerHTML = `${Math.floor(response.main.temp_min - 273.15)}°C`;
-    })
+  console.log("cityValue: " + cityValue);
+  console.log("name : " + name);
+
+  cityName.innerHTML = cityValue;
+
+  uraxInfo(name); //urax의 정보를 가져온다.
+}
 
 
-    
-  },
-});
+function uraxInfo(name){
+
+  // const cityurax = name; //서울은 기본값
+  // let cityurax = 'busan';
+  let cityurax = name;
+console.log("!!!!!cityurax: "+ cityurax);
+
+  let url = API_URL + cityurax + '&appid=' + API_KEY;
+console.log("URL:::::::" + url);
+
+  $.ajax({
+    url: url,
+    dataType: 'json',
+    type: 'GET',
+    async: 'false',
+    success: function (response) {
+      console.log(response);
+      console.log('원래 현재온도 : ' + (response.main.temp - 273.15));
+      console.log('최고온도 : ' + (response.main.temp_max - 273.15));
+      console.log('최저 온도: ' + (response.main.temp_min - 273.15));
+  
+      console.log('현재습도 : ' + response.main.humidity);
+      console.log('날씨 : ' + response.weather[0].main);
+      console.log('상세날씨설명 : ' + response.weather[0].description);
+      console.log('날씨 이미지 : ' + response.weather[0].icon);
+      console.log('바람   : ' + response.wind.speed);
+      console.log('나라   : ' + response.sys.country);
+      console.log('도시이름  : ' + response.name);
+      console.log('구름  : ' + response.clouds.all + '%');
+  
+      temp.innerHTML = `${Math.floor(response.main.temp - 273.15)}°C`;
+      humidity.innerHTML = response.main.humidity;
+      wind.innerHTML = response.wind.speed;     
+
+
+      btnMax.addEventListener("click", function(){
+        btnMax.style.backgroundColor = "white";
+        btnMin.style.backgroundColor = "#FCEDF1";
+        now.style.backgroundColor = "#FCEDF1";
+
+        console.log('BtnMax click!')
+        tempMaxMinName.innerHTML = "최고온도";
+        temp.innerHTML = `${Math.floor(response.main.temp_max - 273.15)}°C`;
+      })
+      
+      btnMin.addEventListener("click", function(){
+        btnMax.style.backgroundColor = "#FCEDF1";
+        btnMin.style.backgroundColor = "white";
+        now.style.backgroundColor = "#FCEDF1";
+
+        btnMin.style.backgroundColor = "white";
+        console.log('BtnMin click!')
+        tempMaxMinName.innerHTML = "최저온도";
+        temp.innerHTML = `${Math.floor(response.main.temp_min - 273.15)}°C`;
+      })  
+
+      now.addEventListener('click', function(){
+        btnMax.style.backgroundColor = "#FCEDF1";
+        btnMin.style.backgroundColor = "#FCEDF1";
+        now.style.backgroundColor = "white";
+      })
+
+
+    },
+  });
+}
+
