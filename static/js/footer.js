8bd2ebba765639e1be62fa511cb3c7847a5463a9
DESKTOP-C4630N1\sooy9 <amole2315@naver.com>
2022-10-26 11:39:12 +0900

    footer 수정

--- static/js/footer.js
+++ static/js/footer.js
@@ -1,3 +1,8 @@
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+
+
+
 function shareTwitter() {
     var sendText = "1080 in italy"; // 전달할 텍스트
     var sendUrl = "devpad.tistory.com/"; // 전달할 URL
@@ -10,24 +15,24 @@ function shareFacebook() {
 }
 
 
-// function shareKakao() {
+function shareKakao() {
  
-//     // 사용할 앱의 JavaScript 키 설정
-//     Kakao.init('cd08c92ff47abb48747e75288b10a427');
+    // 사용할 앱의 JavaScript 키 설정
+    Kakao.init('cd08c92ff47abb48747e75288b10a427');
    
-//     // 카카오링크 버튼 생성
-//     Kakao.Link.createDefaultButton({
-//       container: '#btnKakao', // 카카오공유버튼ID
-//       objectType: 'feed',
-//       content: {
-//         title: "1080 in italy", // 보여질 제목
-//         description: "이탈리아 관광을 위한 페이지입니다", // 보여질 설명
-//         imageUrl: "localhost:8300/", // 콘텐츠 URL
-//         link: {
-//            mobileWebUrl: "localhost:8300/",
-//            webUrl: "localhost:8300/"
-//         }
-//       }
-//     });
-//   }
+    // 카카오링크 버튼 생성
+    Kakao.Link.createDefaultButton({
+      container: '#btnKakao', // 카카오공유버튼ID
+      objectType: 'feed',
+      content: {
+        title: "1080 in italy", // 보여질 제목
+        description: "이탈리아 관광을 위한 페이지입니다", // 보여질 설명
+        imageUrl: "localhost:8300/", // 콘텐츠 URL
+        link: {
+           mobileWebUrl: "localhost:8300/",
+           webUrl: "localhost:8300/"
+        }
+      }
+    });
+  }
   
